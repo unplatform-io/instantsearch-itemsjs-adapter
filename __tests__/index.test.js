ce927d18872d62e54e9fb19303814992b7b8a808
hugosmeets <hugosmeets@users.noreply.github.com>
2021-10-18 14:12:44 +0200

    fix eslint error

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
-import {sum, InstantSearchItemsjsAdapter} from '../src/index'
+import { sum, InstantSearchItemsjsAdapter } from "../src/index";
 
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
