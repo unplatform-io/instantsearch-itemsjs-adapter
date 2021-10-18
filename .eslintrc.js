ee5b29fbc2c4378fb72292313d2eb05bdab0f4c0
hugosmeets <62993294+hugosmeets@users.noreply.github.com>
2021-10-18 11:25:26 +0200

    Eslint prettier (#2)

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  env: {
+    node: true,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export default function InstantSearchItemsjsAdapter(name:string, b:number) {
-    return name + " "+ b ;
+export default function InstantSearchItemsjsAdapter(name: string, b: number) {
+  return name + " " + b;
 }
