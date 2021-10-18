fec4c58ee11793ff342cf91071d21427f602d211
hugosmeets <hugosmeets@users.noreply.github.com>
2021-10-18 13:54:40 +0200

    install jest

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+import {sum, InstantSearchItemsjsAdapter} from '../src/index'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds "test" + 2 to equal "test 2"', () => {
+  expect(InstantSearchItemsjsAdapter("test", 2)).toBe("test 2");
+});
