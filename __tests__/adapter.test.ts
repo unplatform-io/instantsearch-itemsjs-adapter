b9960543540fa1b084af0941850876c3ae42b2aa
Stan Rutten <stanrutten242@hotmail.com>
2021-11-01 13:27:47 +0100

    PR request changes for adapter

--- __tests__/adapter.test.ts
+++ __tests__/adapter.test.ts
@@ -1,19 +1,24 @@
-import productsState from "./products.json";
+import products from "./products.json";
 import { createIndex, performSearch } from "../src/adapter";
 import getSearchClient from "../src/adapter";
 import adaptresponse from "./adaptresponse.json";
+import { SearchResponse } from "@algolia/client-search";
+import { MultipleQueriesResponse } from "@algolia/client-search";
+import { MultipleQueriesQuery } from "@algolia/client-search";
+import { ItemsJsOptions } from "../src/itemsjsInterface";
 
 const per_page = 4;
 const query = "";
 const page = 1;
-const options = {
+
+const options: ItemsJsOptions = {
   searchableFields: ["title"],
   per_page: per_page,
   query: query,
   page: page,
 };
 
-const req = [
+const request: MultipleQueriesQuery[] = [
   {
     indexName: "instant_search",
     params: {
@@ -29,25 +34,18 @@ const req = [
 ];
 
 describe("createIndex", () => {
-  it("Creates a itemsjs index", () => {
-    const response = createIndex(productsState, options);
+  it("Creates an Itemsjs index", () => {
+    const response: SearchResponse = createIndex(products, options);
 
-    const totalNumberOfPages = Math.ceil(
-      response.nbHits / response.hitsPerPage
-    );
+    const totalNumberOfPages: number = Math.ceil(products.length / per_page);
 
+    expect(response.hits[0].objectID).toBe(1);
     expect(response.hits.length).toBe(per_page);
-    expect(response.hits[0].id).toBe(1);
-    expect(response.hits[0].price).toBe(109.95);
-    expect(response.hits[0].title).toBe(
-      "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-    );
-
     expect(response.page).toBe(page - 1);
     expect(response.nbPages).toBe(totalNumberOfPages);
     expect(response.hitsPerPage).toBe(per_page);
-    expect(response.nbHits).toBe(productsState.length);
-    expect(typeof response.processingTimeMS).toEqual("number");
+    expect(response.nbHits).toBe(products.length);
+    expect(response.processingTimeMS).toBeGreaterThanOrEqual(0);
     expect(response.exhaustiveNbHits).toBe(true);
     expect(response.query).toBe("");
     expect(response.params).toBe("");
@@ -55,10 +53,11 @@ describe("createIndex", () => {
 });
 
 describe("performSearch", () => {
-  it("Performs a search", () => {
-    const performsearch = performSearch(req);
-    // If you exclude this return statement, your test will complete before the promise returned.
-    return expect(performsearch).resolves.toStrictEqual({
+  it("Performs a search", async () => {
+    const performsearch: Readonly<Promise<MultipleQueriesResponse<object>>> =
+      performSearch(request);
+
+    await expect(performsearch).resolves.toStrictEqual({
       results: [adaptresponse],
     });
   });
@@ -66,9 +65,10 @@ describe("performSearch", () => {
 
 describe("getSearchClient", () => {
   it("Creates an Instantsearch searchclient", () => {
-    const getsearchclient = getSearchClient(productsState, options);
+    const getsearchclient = getSearchClient(products, options);
+
     // const search = (queries) => performSearch(queries)
-    // expect(abc.search).toBe(search) // TODO
+    // expect(getsearchclient.search).toBe(search) // TODO
     expect(getsearchclient.searchForFacetValues).toThrow("Not implemented");
   });
 });
