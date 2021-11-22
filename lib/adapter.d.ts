3af6bf63bfe2859ee1b807b8373bd5a45856c008
Stan Rutten <stanrutten242@hotmail.com>
2021-11-22 15:47:31 +0100

    Remove lib from repository

--- lib/adapter.d.ts
+++
@@ -1,5 +0,0 @@
-import { MultipleQueriesResponse, MultipleQueriesQuery } from "@algolia/client-search";
-import { ItemsJsOptions, SearchClient } from "./itemsjsInterface";
-export declare function getSearchClient(): SearchClient;
-export declare function createIndex(data: object, options: ItemsJsOptions): void;
-export declare function performSearch(request: MultipleQueriesQuery[]): Readonly<Promise<MultipleQueriesResponse<object>>>;
