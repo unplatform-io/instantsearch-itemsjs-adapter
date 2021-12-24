9734535d3ae49c77d3642b018b35555d8c20c21b
Stan Rutten <stanrutten242@hotmail.com>
2021-12-24 09:05:12 +0100

    ASUPF-240 - #33: Minify build (#35)
    * Faster buildtime and minify build
    
    * version 1.1.5 - ESLint
    
    * Add type checking at build time

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+// https://github.com/egoist/tsup/blob/main/src/options.ts
+export default defineConfig({
+  entry: ["src/*"],
+  target: "node14.18.1",
+  minify: true,
+  bundle: true,
+  outDir: "lib",
+  clean: true,
+});
